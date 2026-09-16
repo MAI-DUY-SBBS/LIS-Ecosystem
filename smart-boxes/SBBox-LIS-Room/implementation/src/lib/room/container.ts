4ff0a9887944f0af35a7b43fd947463fdf328d3a
Mai The Duy <vnwfpb@gmail.com>
2026-09-16 18:56:50 +0700

    Connect LIS Room services to PostgreSQL

--- smart-boxes/SBBox-LIS-Room/implementation/src/lib/room/container.ts
+++ smart-boxes/SBBox-LIS-Room/implementation/src/lib/room/container.ts
@@ -2,24 +2,38 @@ import { PostgresRoomRepository } from "./postgres-repository";
 
 import { PostgresRoomMembershipRepository } from "./postgres-membership-repository";
 
+import { PostgresRoomMessageRepository } from "./postgres-message-repository";
+
 import { QRService } from "./qr-service";
 
 import { MembershipService } from "./membership-service";
 
+import { MessageService } from "./message-service";
+
 import { RoomService } from "./service";
 
-const roomRepository = new PostgresRoomRepository();
+const roomRepository =
+  new PostgresRoomRepository();
 
 const roomMembershipRepository =
   new PostgresRoomMembershipRepository();
 
-export const roomService = new RoomService(
-  roomRepository,
-);
+const roomMessageRepository =
+  new PostgresRoomMessageRepository();
 
-export const membershipService = new MembershipService(
-  roomRepository,
-  roomMembershipRepository,
-);
+export const roomService =
+  new RoomService(roomRepository);
 
-export const qrService = new QRService();
+export const membershipService =
+  new MembershipService(
+    roomRepository,
+    roomMembershipRepository,
+  );
+
+export const messageService =
+  new MessageService(
+    roomMessageRepository,
+  );
+
+export const qrService =
+  new QRService();
